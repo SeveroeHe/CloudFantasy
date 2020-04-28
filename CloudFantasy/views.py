bc0dce8fa778e23f4b02f7d8792e1b3ccbbec1c9
SeveroeHe <qiyanghe0409@gmail.com>
2020-04-28 00:30:12 -0700

    Finish main page and sub page 1

---
+++ CloudFantasy/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+#please specify package usage
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+from django.core import serializers
+import re,json
+from django.utils import timezone
+from django.utils.html import format_html
+from django.db.models import Q
+# Create your views here.
+
+
+'''
+Home Page
+'''
+def home(request):
+	return render(request, 'CloudFantasy/home.html', {})
+
+def director(request):
+	return render(request, 'CloudFantasy/director.html', {'cur1':'cur_page'})
