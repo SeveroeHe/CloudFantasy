b47466e9d04d515df34917aefb82b3b0ee83c6c4
SeveroeHe <qiyanghe0409@gmail.com>
2020-05-19 22:30:42 -0700

    Final style fix

--- CloudFantasy/views.py
+++ CloudFantasy/views.py
@@ -37,23 +37,23 @@ def tickets(request):
 	return render(request, 'CloudFantasy/ticket.html', {'cur4':'cur_page'})
 
 def films(request):
-	return render(request, 'CloudFantasy/film1.html', {'cur2':'cur_page', "dk": "dk"})
+	return render(request, 'CloudFantasy/film1.html', {'cur2':'cur_page', "dk": "dk", "film_footer": "footer-red"})
 
 def film1(request):
-	return render(request, 'CloudFantasy/film1.html', {'cur2':'cur_page', "dk": "dk"})
+	return render(request, 'CloudFantasy/film1.html', {'cur2':'cur_page', "dk": "dk", "film_footer": "footer-red"})
 
 def film2(request):
-	return render(request, 'CloudFantasy/film2.html', {'cur2':'cur_page'})
+	return render(request, 'CloudFantasy/film2.html', {'cur2':'cur_page', "film_footer": "footer-red"})
 
 def film3(request):
-	return render(request, 'CloudFantasy/film3.html', {'cur2':'cur_page'})
+	return render(request, 'CloudFantasy/film3.html', {'cur2':'cur_page', "film_footer": "footer-red"})
 
 def film4(request):
-	return render(request, 'CloudFantasy/film4.html', {'cur2':'cur_page'})
+	return render(request, 'CloudFantasy/film4.html', {'cur2':'cur_page', "film_footer": "footer-red"})
 
 def film5(request):
-	return render(request, 'CloudFantasy/film5.html', {'cur2':'cur_page'})
+	return render(request, 'CloudFantasy/film5.html', {'cur2':'cur_page', "film_footer": "footer-red"})
 
 def bts(request):
-	return render(request, 'CloudFantasy/bts.html', {'cur5':'cur_page'})
+	return render(request, 'CloudFantasy/bts.html', {'cur5':'cur_page', "film_footer": "footer-red"})
 
